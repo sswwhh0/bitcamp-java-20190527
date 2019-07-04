690a1db6f973fe58783204783db11187f010808a
shinwoohyuk <s_swwhh@naver.com>
2019-07-04 16:18:55 +0900

    bitcamp-java-application v04 버전 완료

---
+++ bitcamp-java-application/v04/src/main/java/com/eomcs/lms/App2.java
@@ -0,0 +1,15 @@
+//애플리케이션 메인 클래스
+// -> 애플리케이션을 실행할 때 이 클래스를 실행한다.
+package com.eomcs.lms;
+
+public class App2 {
+  public static void main(String[] args) {
+    System.out.println("번호 : 1");
+    System.out.println("이름: 홍길동");
+    System.out.println("이메일: hong@test.com");
+    System.out.println("암호: 1111");
+    System.out.println("사진: hong.png");
+    System.out.println("전화: 1111-2222");
+    System.out.println("가입일: 2019-01-01");
+  }
+}
