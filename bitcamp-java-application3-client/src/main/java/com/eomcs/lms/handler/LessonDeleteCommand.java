b1bfd61f8947400a3e4cef832afd21b89ac325ed
shinwoohyuk <s_swwhh@naver.com>
2019-08-12 12:03:39 +0900

    bitcamp-java-0812-12:03

---
+++ bitcamp-java-application3-client/src/main/java/com/eomcs/lms/handler/LessonDeleteCommand.java
@@ -0,0 +1,45 @@
+package com.eomcs.lms.handler;
+
+import com.eomcs.lms.dao.LessonDao;
+import com.eomcs.util.Input;
+
+public class LessonDeleteCommand implements Command {
+
+  private LessonDao lessonDao;
+  private Input input;
+
+  public LessonDeleteCommand(Input input, LessonDao lessonDao) {
+    this.input = input;
+    this.lessonDao = lessonDao;
+  }
+
+  @Override
+  public void execute() {
+    int no = input.getIntValue("번호? ");
+
+    try {
+      if (lessonDao.delete(no) > 0) {
+      // 사용자가 입력한 번호를 가지고 목록에서 그 번호에 해당하는 Lesson 객체를 찾는다.
+      System.out.println("데이터를 삭제하였습니다.");
+      } else {
+        System.out.println("해당 데이터가 없습니다.");
+      }
+    } catch(Exception e) {
+      System.out.println("해당 번호의 데이터가 없습니다!");
+      System.out.println(e.getMessage());
+    }
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
