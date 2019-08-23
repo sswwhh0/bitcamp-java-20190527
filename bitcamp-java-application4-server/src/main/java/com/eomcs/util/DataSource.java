076f65460a57d66e52aff54b1e06e933a4a1f913
shinwoohyuk <s_swwhh@naver.com>
2019-08-23 16:30:39 +0900

    bitcamp-java-0823-16:30

--- bitcamp-java-application4-server/src/main/java/com/eomcs/util/DataSource.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/util/DataSource.java
@@ -3,9 +3,10 @@ package com.eomcs.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 // DAO가 사용할 커넥션 객체를 생성해주는 역할
-public class ConnectionFactory {
+public class DataSource {
   String jdbcDriver;
   String jdbcUrl;
   String username;
@@ -14,8 +15,11 @@ public class ConnectionFactory {
   // 스레드 별로 커넥션 객체를 사용하기 위해
   // 햔재 스레드의 값을 꺼내고 넣을 수 있는 도구를 준비한다.
   ThreadLocal<Connection> localConnection = new ThreadLocal<>();
+  
+  //
+  ArrayList<Connection> conPool = new ArrayList<>();
 
-  public ConnectionFactory(
+  public DataSource(
       String jdbcDriver, String jdbcUrl, String username, String password) {
     this.jdbcDriver = jdbcDriver;
     this.jdbcUrl = jdbcUrl;
@@ -29,9 +33,15 @@ public class ConnectionFactory {
     Connection con = localConnection.get();
 
     if(con == null) { // 없다면 새로 만들어 현재 스레드에 보관한다.
-      Class.forName(jdbcDriver);
-      con = new TxConnection(DriverManager.getConnection(
-          jdbcUrl, username, password));
+      
+      if(conPool.size() > 0) { // 먼저 커넥션풀에서 커넥션을 찾아보고 있으면 꺼낸다.
+        con = conPool.remove(0);
+      } else { // 커넥션 풀
+        Class.forName(jdbcDriver);
+        con = new TxConnection(DriverManager.getConnection(
+            jdbcUrl, username, password));
+        System.out.println("새 커넥션 만들어 사용!");
+      }
 
       //생성한 커넥션을 리턴하기 전에 ThreadLocal 도구를 사용하여 현재 스레드에 보관한다.
       localConnection.set(con);
@@ -49,6 +59,10 @@ public class ConnectionFactory {
       } catch(SQLException e) {
       }
       localConnection.remove();
+      
+      // 스레드에서 제거한 후, 커넥션 객체를 다시 커넥션풀에 저장한다.
+      conPool.add(con);
+      System.out.println("커넥션풀에 다시 저장");
     }
   }
   
