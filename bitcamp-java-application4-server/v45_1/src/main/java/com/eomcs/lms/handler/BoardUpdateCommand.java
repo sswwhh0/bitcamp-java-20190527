1cd3db4e4b4866345c689f717f32c99b63c1f422
shinwoohyuk <s_swwhh@naver.com>
2019-08-29 16:15:38 +0900

    bitcamp-java-0829-16:15

--- bitcamp-java-application4-server/v45_1/src/main/java/com/eomcs/lms/handler/BoardUpdateCommand.java
+++ bitcamp-java-application4-server/v45_1/src/main/java/com/eomcs/lms/handler/BoardUpdateCommand.java
@@ -6,11 +6,11 @@ import com.eomcs.lms.dao.BoardDao;
 import com.eomcs.lms.domain.Board;
 import com.eomcs.util.Input;
 
-public class BoardDetailCommand implements Command {
+public class BoardUpdateCommand implements Command {
 
   private BoardDao boardDao;
 
-  public BoardDetailCommand(BoardDao boardDao) {
+  public BoardUpdateCommand(BoardDao boardDao) {
     this.boardDao = boardDao;
   }
 
@@ -18,21 +18,30 @@ public class BoardDetailCommand implements Command {
   public void execute(BufferedReader in, PrintStream out) {
 
     try {
-      //클라이언트에게 번호를 요구하여 받는다.
       int no = Input.getIntValue(in, out, "번호? ");
       Board board = boardDao.findBy(no);
-
+      
       if (board == null) {
         out.println("해당 번호의 데이터가 없습니다!");
         return;
       }
-      boardDao.increaseViewCount(no);
+
+      // 사용자로부터 변경할 값을 입력 받는다.
+      Board data = new Board();
+      data.setNo(no);
       
-      out.printf("내용: %s\n", board.getContents());
-      out.printf("작성일: %s\n", board.getCreatedDate());
+      String str = Input.getStringValue(in, out, "내용? ");
+      
+      if (str.length() > 0) {
+        data.setContents(str);
+        boardDao.update(data);
+        out.println("데이터를 변경하였습니다.");
+      } else {
+        out.println("데이터 변경을 취소합니다");
+      }
       
     } catch(Exception e) {
-      out.println("데이터 조회에 실패했습니다!");
+      out.println("데이터 변경에 실패했습니다!");
       System.out.println(e.getMessage());
     }
 
