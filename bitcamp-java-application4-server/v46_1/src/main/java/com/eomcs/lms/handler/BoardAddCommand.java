fb75cb385b0d9d8fda4db693f2c6a4856b18b645
shinwoohyuk <s_swwhh@naver.com>
2019-08-30 15:46:11 +0900

    bitcamp-java-0830-15:46

---
+++ bitcamp-java-application4-server/v46_1/src/main/java/com/eomcs/lms/handler/BoardAddCommand.java
@@ -0,0 +1,38 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.util.Scanner;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import com.eomcs.lms.dao.BoardDao;
+import com.eomcs.lms.domain.Board;
+import com.eomcs.util.Input;
+
+public class BoardAddCommand implements Command {
+
+  Scanner scan;
+  private SqlSessionFactory sqlSessionFactory;
+
+  public BoardAddCommand(SqlSessionFactory sqlSessionFactory) {
+    this.sqlSessionFactory = sqlSessionFactory;
+  }
+
+  @Override
+  public void execute(BufferedReader in, PrintStream out) {
+
+    try(SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      BoardDao boardDao = sqlSession.getMapper(BoardDao.class);
+      Board board = new Board();
+      board.setContents(Input.getStringValue(in, out, "내용? "));
+      
+      boardDao.insert(board);
+      out.println("저장하였습니다.");
+      
+    } catch (Exception e) {
+      out.println("데이터 저장에 실패했습니다!");
+      System.out.println(e.getMessage());
+    }
+  }
+
+}
